fa278353b0b369a6f713e4102cb8c4f213d7c898
Nithisha R <lathanithisha@gmail.com>
2026-06-08 14:30:00 +0530

    reponsive design

---
+++ day.java
@@ -0,0 +1,9 @@
+import java.util.scanner;
+public class day{
+    public static void main{
+        scanner sc = new scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=0;i<n;i++);
+        System.out.println(int i);
+    }
+} 
